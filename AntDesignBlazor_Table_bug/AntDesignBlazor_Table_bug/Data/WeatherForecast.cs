b1ed0f3d286cb300d36be87d97f2e2e6efe48775
zhusir <smallprogram@foxmail.com>
2022-02-09 01:21:13 +0800

    update

--- AntDesignBlazor_Table_bug/AntDesignBlazor_Table_bug/Data/WeatherForecast.cs
+++ AntDesignBlazor_Table_bug/AntDesignBlazor_Table_bug/Data/WeatherForecast.cs
@@ -3,17 +3,12 @@ namespace AntDesignBlazor_Table_bug.Data
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
-
-        public int TemperatureF { get; set; } 
+        public int TemperatureF { get; set; }
         public string? Column1 { get; set; }
         public string? Column2 { get; set; }
         public string? Column3 { get; set; }
         public string? Column4 { get; set; }
-
         public string? Summary { get; set; }
-        
     }
 }
